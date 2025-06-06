58616956206b2ba49ce2e1c5c15403dc4240a688
Vik (Ritvik Ayyer) <163168268+viks1ayyer@users.noreply.github.com>
2025-06-06 20:11:35 +0800

    Add files via upload

---
+++ 01_CSVtoSQL_Nutritional_Facts_Database/USDA.sql
@@ -0,0 +1,11 @@
+CREATE TABLE nutrition (
+    fdc_id INT AUTO_INCREMENT PRIMARY KEY,
+    brand_name VARCHAR(255),
+    serving_size FLOAT DEFAULT 100,
+    calories FLOAT,
+    ingredients TEXT
+    protein INT
+    fat INT
+    carbs INT
+    sugar INT
+);
